cff58213e1432550bd8b76705e1d8e7943f0991b
Isaac Augusto <isaac.augusto@exitgames.com>
2023-08-19 11:55:24 -0300

    Added Player Spawner

---
+++ Assets/Scripts/Player/PlayerSpawner.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    [SerializeField] private PlayerMovement _player;
+
+    private void OnEnable()
+    {
+        LevelManager.OnLevelStart.AddListener(SpawnPlayer);
+    }
+
+    private void OnDisable()
+    {
+        LevelManager.OnLevelStart.RemoveListener(SpawnPlayer);
+    }
+
+    private void SpawnPlayer()
+    {
+        Instantiate(_player, transform.position, quaternion.identity);
+    }
+}
